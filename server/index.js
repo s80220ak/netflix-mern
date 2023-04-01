99836b4408b3be33a8f8aee7b7338ffb2d9036bf
Jia <test@gmail.com>
2023-04-01 23:50:39 +0800

    cors adjust

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import http from "http";
 import mongoose from "mongoose";
 import "dotenv/config";
 import routes from "./src/routes/index.js";
 
 const app = express();
+const corsOptions = {
+  origin: [
+    "https://netflix-mern-s80220ak.vercel.app/",
+    "http://localhost:3000",
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
